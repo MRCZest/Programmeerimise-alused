fc383fe8016da5d6015de657aa4ddb19daea92fc
opilane <marcusgrunberg@gmail.com>
2025-11-18 13:52:19 +0200

    Matemaatika ülesande lõpetamine koos kontrollidega

--- 04_matemaatilised_avaldised/ex01_matemaatilised_avaldised/solution.py
+++ 04_matemaatilised_avaldised/ex01_matemaatilised_avaldised/solution.py
@@ -4,9 +4,9 @@ import math
 
 def sum_and_difference(num_a: int, num_b: int) -> tuple:
     """Return the sum and difference of given variables num_a and num_b."""
-    sum = num_a + num_b
+    addition_result = num_a + num_b
     difference = num_a - num_b
-    return sum, difference
+    return addition_result, difference
 
 
 def float_division(num_a: int, num_b: int) -> float:
@@ -17,16 +17,16 @@ def float_division(num_a: int, num_b: int) -> float:
 
 def integer_division(num_a: int, num_b: int) -> int:
     """Divide given variables num_a and num_b and return the result rounded down."""
-    division = num_a // num_b
-    return division
+    integer_division = num_a // num_b
+    return integer_division
 
 
 def powerful_operations(num_a: int, num_b: int) -> tuple:
     """Return the product of given variables, num_a to the power of num_b and the remainder of division of variables."""
-    multiply_numbers = num_a * num_b
+    multiplication = num_a * num_b
     power = num_a ** num_b
     remainder = num_a % num_b
-    return multiply_numbers, power, remainder
+    return multiplication, power, remainder
 
 
 def find_average(num_a: int, num_b: int) -> float:
@@ -37,29 +37,69 @@ def find_average(num_a: int, num_b: int) -> float:
 
 def area_of_a_circle(radius: float) -> float:
     """Calculate and return the area of a circle."""
-    circle_area = math.pi * radius ** 2
+    circle_area = 3.14 * radius ** 2
     return circle_area
 
 
 def area_of_an_equilateral_triangle(side_length: float) -> float:
     """Calculate and return the area of an equilateral triangle."""
     triangle_area = math.sqrt(3) / 4 * side_length ** 2
-    return triangle_area
+    return round(triangle_area)
 
 
 def calculate_discriminant(a: int, b: int, c: int) -> int:
     """Calculate discriminant with given variables and return the result."""
-    # Write your code here
+    discriminant = b ** 2 - 4 * a * c
     return discriminant
 
 
 def calculate_hypotenuse_length(a: int, b: int) -> float:
     """Return the length of hypotenuse when the lengths of the catheti are given."""
-    # Write your code here
-    return c
+    hypotenuse = math.sqrt(a ** 2 + b ** 2)
+    return round(hypotenuse)
 
 
 def calculate_cathetus_length(a: int, c: int) -> float:
     """Return the length of cathetus when the lengths of the second cathetus and hypotenuse are given."""
-    # Write your code here
-    return b
+    cathetus = math.sqrt(c ** 2 - a ** 2)
+    return round(cathetus)
+
+if __name__ == '__main__':
+    addition_result, difference = sum_and_difference(5, 6)
+    assert addition_result == 11
+    assert difference == -1
+
+    float_division_result = float_division(10, 10)
+    assert isinstance(float_division_result, float)
+    assert 0.99 < float_division_result < 1.01
+    float_division_result = float_division(10, 2)
+    assert 4.99 < float_division_result < 5.01
+
+    integer_division_result = integer_division(10, 10)
+    assert isinstance(integer_division_result, int)
+    assert integer_division_result == 1
+    integer_division_result = integer_division(10,2)
+    assert integer_division_result == 5
+
+    multiplication, power, remainder = powerful_operations(3, 4)
+    assert multiplication == 12
+    assert power == 81
+    assert remainder == 3
+
+    average_result = find_average(10, 5)
+    assert average_result == 7.5
+
+    circle_area_result = area_of_a_circle(10)
+    assert circle_area_result == 314
+
+    triangle_area_result = area_of_an_equilateral_triangle(8)
+    assert triangle_area_result == 28
+
+    discriminant_result = calculate_discriminant(8, 20, 12)
+    assert discriminant_result == 16
+
+    hypotenuse_result = calculate_hypotenuse_length(10, 8)
+    assert hypotenuse_result == 13
+
+    cathetus_result = calculate_cathetus_length(10, 16)
+    assert cathetus_result == 12
